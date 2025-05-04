9f860a7851d8d7e06e64e81359db9d5828b21f27
jasbx222 <gloop216@gmail.com>
2025-05-04 15:31:56 +0300

    Update Logo.jsx

--- src/app/components/sidebar/Logo.jsx
+++ src/app/components/sidebar/Logo.jsx
@@ -2,7 +2,7 @@ import logo from "../../../../public/img/logo.jpg";
 import Image from "next/image";
 export const Logo = () => {
   return (
-    <div className="absolute bottom-0 left-0 w-full p-4 bg-gray-900 text-center text-gray-200">
+    <div className="absolute bottom-0 lg:hidden left-0 w-full p-4 bg-gray-900 text-center text-gray-200">
       <p className="text-sm">تطبيق إدارة الأصول</p>
       <p className="text-sm">جميع الحقوق محفوظة &copy; 2025</p>
       <Image
